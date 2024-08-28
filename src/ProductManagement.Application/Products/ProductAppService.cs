006c54dafc50c8b4b9398e87f3489d7db2e40844
pedroizquierdobueno <pedro.izquierdobueno@efclif.com>
2024-08-28 11:21:28 +0200

    Listing the product data - ProductAppService

---
+++ src/ProductManagement.Application/Products/ProductAppService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Products;
+
+public class ProductAppService(IRepository<Product, Guid> productRepository) : ProductManagementAppService, IProductAppService
+{
+	private readonly IRepository<Product, Guid> _productRepository = productRepository;
+
+	public async Task<PagedResultDto<ProductDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+	{
+		IQueryable<Product> queryable = await _productRepository
+								.WithDetailsAsync(p => p.Category);
+
+		queryable = queryable
+						.Skip(input.SkipCount)
+						.Take(input.MaxResultCount)
+						.OrderBy(input.Sorting ?? nameof(Product.Name));
+
+		List<Product> products = await AsyncExecuter.ToListAsync(queryable);
+
+		long count = await _productRepository.GetCountAsync();
+
+		return new PagedResultDto<ProductDto>(count, ObjectMapper.Map<List<Product>, List<ProductDto>>(products));
+	}
+}
